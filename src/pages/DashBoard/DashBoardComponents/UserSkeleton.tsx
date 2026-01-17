98c9ac241447be820bc6034789b988a91fa8e581
satyamsingh7417 <satyamsingh7417@gmail.com>
2026-01-17 11:19:45 +0530

    .

--- src/pages/DashBoard/DashBoardComponents/UserSkeleton.tsx
+++ src/pages/DashBoard/DashBoardComponents/UserSkeleton.tsx
@@ -1,6 +1,6 @@
 const UserSkeleton = () => {
   return (
-    <div className="flex items-center justify-between rounded-xl bg-slate-50 px-4 py-3 animate-pulse">
+    <div className="flex items-center justify-between rounded-xl bg-stone-200 px-4 py-3 animate-pulse">
       <div className="space-y-2">
         <div className="h-3 w-32 rounded bg-slate-200" />
         <div className="h-2 w-40 rounded bg-slate-200" />
